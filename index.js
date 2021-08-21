58c85c9fd549d649f34ae709e01d813df4df497d
harshitmarmat <65807810+harshitmarmat@users.noreply.github.com>
2021-08-21 13:35:21 +0530

    Update index.js

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const PORT = process.env.PORT || 9000;
 
 const Contact = require('./models/contact');
 const db = require('./config/mongoose');
+const { PRIORITY_ABOVE_NORMAL } = require('constants');
 db();
 const app = express();
 
@@ -14,23 +15,6 @@ app.use(express.urlencoded());
 
 app.use(express.static('assets'));
 
-
-var contactList = [
-    {
-        name : "Harshit",
-        phone : "7823897249" 
-    },
-    {
-        name : "Nishu",
-        phone : "1234567890" 
-    },
-    {
-        name : "Mummy",
-        phone : "0987654321"
-    }
-];
-
-
 app.get('/',function(req,res){
 
     Contact.find({},function(err,contacts){
@@ -39,23 +23,14 @@ app.get('/',function(req,res){
             return;
         }
         return res.render('home',{
-            // title:"My contact Lists",
             contact_List : contacts
         });
     })
-
-
-    // return res.render('home',{
-    //     title:"My contact Lists",
-    //     contact_List : contactList
-    // });
 });
 
 
 
 app.post('/create-contact',function(req,res){
-    // contactList.push(req.body);
-    
     Contact.create({
         name : req.body.name,
         phone : req.body.phone
@@ -67,8 +42,6 @@ app.post('/create-contact',function(req,res){
         console.log('****************',newAccount);
         return res.redirect('back');
     });
-
-    // return res.redirect('back');
 });
 
 app.get('/delete-contact/',function(req,res){
@@ -81,48 +54,7 @@ app.get('/delete-contact/',function(req,res){
         }
         return res.redirect('back');
     });
-
-
-    // let contactIndex = contactList.findIndex(contact=> contact.phone == phone);
-
-    // if(contactIndex!=-1){
-    //     contactList.splice(contactIndex,1);
-    // }
-    // return res.redirect('back');
 });
-
-// app.post("/update",function(req,res){
-//     let id = req.body.name;
-//     console.log(id);
-//     let value = req.body.phone;
-//     console.log(req.body);
-//     console.log(Contact.find({name : id}));
-
-//     // Contact.findByIdAndUpdate(id,{
-//     //     name : id,
-//     //     phone : value
-//     // },function(err){
-//     //     if(err){
-//     //         console.log('Error in updating the contact from DB',err);
-//     //         return ;
-//     //     }
-//     //     return res.redirect('back');
-//     // });
-// });
-
-
-// app.post("/update-name",function(req,res){
-//     let currentName = req.body.name;
-//     let currentPhone = req.body.phone;
-//     for( let i of contactList){
-//         if(currentPhone == i.phone){
-//             i.name = currentName;
-//             return res.redirect('back');
-//         }
-//     }
-//     return res.redirect('program');
-// });
-
 
 app.listen(PORT , function(err){
     if(err){
